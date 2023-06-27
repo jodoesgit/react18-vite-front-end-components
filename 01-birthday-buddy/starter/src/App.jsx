bb35ffac01b47b1842e788fe0447ce4cfe681853
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-06-27 16:23:26 -0700

    Created List, which pulls data from Person

--- 01-birthday-buddy/starter/src/App.jsx
+++ 01-birthday-buddy/starter/src/App.jsx
@@ -1,7 +1,14 @@
 import { useState } from "react";
+import data from "./data";
+import List from "./List";
 
 const App = () => {
 	const [people, setPeople] = useState(data);
-	return <h2>Birthday Reminder - Starter</h2>;
+	return (
+		<section className="container">
+			<h3>{people.length} birthdays today</h3>
+			<List people={people} />
+		</section>
+	);
 };
 export default App;
--- 01-birthday-buddy/starter/src/List.jsx
+++ 01-birthday-buddy/starter/src/List.jsx
@@ -1,4 +1,12 @@
-const List = () => {
-	return <div>List</div>;
+import Person from "./Person";
+
+const List = ({ people }) => {
+	return (
+		<section>
+			{people.map((person) => {
+				return <Person key={person.id} {...person} />;
+			})}
+		</section>
+	);
 };
 export default List;
