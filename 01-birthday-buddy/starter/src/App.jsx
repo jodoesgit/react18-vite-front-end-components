b373a4cc87120c3b18742a22c75a814cddea4c58
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-06-27 16:59:02 -0700

    Added styles

--- 01-birthday-buddy/starter/src/App.jsx
+++ 01-birthday-buddy/starter/src/App.jsx
@@ -5,17 +5,19 @@ import List from "./List";
 const App = () => {
 	const [people, setPeople] = useState(data);
 	return (
-		<section className="container">
-			<h3>{people.length} birthdays today</h3>
-			<List people={people} />
-			<button
-				type="button"
-				className="btn btn-block"
-				onClick={() => setPeople([])}
-			>
-				clear all
-			</button>
-		</section>
+		<main>
+			<section className="container">
+				<h3>{people.length} birthdays today</h3>
+				<List people={people} />
+				<button
+					type="button"
+					className="btn btn-block"
+					onClick={() => setPeople([])}
+				>
+					clear all
+				</button>
+			</section>
+		</main>
 	);
 };
 export default App;
