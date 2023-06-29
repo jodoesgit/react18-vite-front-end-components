f0564acb58ba25bed6942794814d96da813bc332
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-06-28 22:01:48 -0700

    Setup basic Review component

--- 03-reviews/starter/src/App.jsx
+++ 03-reviews/starter/src/App.jsx
@@ -1,9 +1,46 @@
 import { useState } from "react";
 import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
 
 const App = () => {
 	const [index, setIndex] = useState(0);
 	const { name, job, image, text } = people[index];
-	return <h2>Reviews Starter</h2>;
+
+	const nextPerson = () => {
+		setIndex((currentIndex) => {
+			const newIndex = currentIndex + 1;
+			return newIndex;
+		});
+	};
+	const prevPerson = () => {
+		setIndex((currentIndex) => {
+			const newIndex = currentIndex - 1;
+			return newIndex;
+		});
+	};
+
+	return (
+		<main>
+			<article className="review">
+				<div className="img-container">
+					<img src={image} alt={name} className="person-img" />
+					<span className="quote-icon">
+						<FaQuoteRight />
+					</span>
+				</div>
+				<h4 className="author">{name}</h4>
+				<p className="job">{job}</p>
+				<p className="info">{text}</p>
+				<div className="btn-container">
+					<button className="prev-btn" onClick={prevPerson}>
+						<FaChevronLeft />
+					</button>
+					<button className="next-btn" onClick={nextPerson}>
+						<FaChevronRight />
+					</button>
+				</div>
+			</article>
+		</main>
+	);
 };
 export default App;
