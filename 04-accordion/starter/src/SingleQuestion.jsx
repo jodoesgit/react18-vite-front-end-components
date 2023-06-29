267c666d41f5257ea193385ff595b180b0da1ed0
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-06-29 16:49:09 -0700

    Added accordion functionality and icons

--- 04-accordion/starter/src/SingleQuestion.jsx
+++ 04-accordion/starter/src/SingleQuestion.jsx
@@ -1,14 +1,18 @@
-import { AiOutlineMinus, AiOutlinePlus } from "react";
+import { AiOutlineMinus, AiOutlinePlus } from "react-icons/ai";
 import { useState } from "react";
 
 const SingleQuestion = ({ title, info }) => {
 	const [showInfo, setShowInfo] = useState(false);
+
 	return (
 		<article className="question">
 			<header>
 				<h5>{title}</h5>
+				<button className="question-btn" onClick={() => setShowInfo(!showInfo)}>
+					{showInfo ? <AiOutlineMinus /> : <AiOutlinePlus />}
+				</button>
 			</header>
-			<p>{info}</p>
+			{showInfo && <p>{info}</p>}
 		</article>
 	);
 };
