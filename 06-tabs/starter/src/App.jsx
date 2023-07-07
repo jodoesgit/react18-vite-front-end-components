80c2faf292bfbb00d41f2660c2ace6943066ec5d
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-07-06 22:41:55 -0700

    Pulled data from API

--- 06-tabs/starter/src/App.jsx
+++ 06-tabs/starter/src/App.jsx
@@ -1,6 +1,30 @@
-const url = 'https://course-api.com/react-tabs-project';
+import { useState, useEffect } from "react";
+
+const url = "https://course-api.com/react-tabs-project";
 
 const App = () => {
-  return <h2>Tabs Starter</h2>;
+	const [isLoading, setIsLoading] = useState(true);
+	const [jobs, setJobs] = useState([]);
+
+	const fetchJobs = async () => {
+		const response = await fetch(url);
+		const newJobs = await response.json();
+		setJobs(newJobs);
+		setIsLoading(false);
+	};
+
+	useEffect(() => {
+		fetchJobs();
+	}, []);
+
+	if (isLoading) {
+		return (
+			<section className="jobs-center">
+				<div className="loading"></div>
+			</section>
+		);
+	}
+
+	return <h2>Tabs Starter</h2>;
 };
 export default App;
