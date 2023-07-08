a89bc83e0d0d2728faed32d165932c76ea5fc747
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-07-08 16:50:19 -0700

    Added slide transitions on carousel

--- 07-slider/starter/src/Carousel.jsx
+++ 07-slider/starter/src/Carousel.jsx
@@ -1,20 +1,40 @@
-import { shortList, list, longList } from "./data";
+import { list } from "./data";
 import { useState } from "react";
 import { FaQuoteRight } from "react-icons/fa";
 import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
 
 const Carousel = () => {
-	const [people, setPeople] = useState(shortList);
+	const [people, setPeople] = useState(list);
+	const [currentPerson, setCurrentPerson] = useState(0);
 
-	const prevSlide = () => {};
-	const nextSlide = () => {};
+	const prevSlide = () => {
+		setCurrentPerson((oldPerson) => {
+			const result = (oldPerson - 1 + people.length) % people.length;
+			return result;
+		});
+	};
+
+	const nextSlide = () => {
+		setCurrentPerson((oldPerson) => {
+			const result = (oldPerson + 1) % people.length;
+			return result;
+		});
+	};
 
 	return (
 		<section className="slider-container">
-			{people.map((person) => {
+			{people.map((person, personIndex) => {
 				const { id, image, name, title, quote } = person;
 				return (
-					<article className="slide" key={id}>
+					<article
+						className="slide"
+						style={{
+							transform: `translateX(${100 * (personIndex - currentPerson)}%)`,
+							opacity: personIndex === currentPerson ? 1 : 0,
+							visibility: personIndex === currentPerson ? 1 : 0,
+						}}
+						key={id}
+					>
 						<img src={image} alt={name} className="person-img" />
 						<h5 className="name">{name}</h5>
 						<p className="title">{title}</p>
