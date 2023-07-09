9609bd797ba31a874b5d184ec10f8c9087054d8b
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-07-08 21:09:25 -0700

    Added Toastify

--- 09-color-generator/starter/src/App.jsx
+++ 09-color-generator/starter/src/App.jsx
@@ -2,6 +2,7 @@ import Form from "./Form";
 import ColorList from "./ColorList";
 import Values from "values.js";
 import { useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
 
 const App = () => {
 	const [colors, setColors] = useState(new Values("#f15025").all(10));
@@ -10,6 +11,7 @@ const App = () => {
 		<main>
 			<Form />
 			<ColorList colors={colors} />
+			<ToastContainer position="top-center" />
 		</main>
 	);
 };
