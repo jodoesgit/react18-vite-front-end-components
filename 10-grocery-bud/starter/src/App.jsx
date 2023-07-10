1e965a2dd270aac0c9519c023d0159810a85de2b
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-07-10 16:43:40 -0700

    Got single item user input to populate the form

--- 10-grocery-bud/starter/src/App.jsx
+++ 10-grocery-bud/starter/src/App.jsx
@@ -1,11 +1,26 @@
 import { useState } from "react";
 import Form from "./Form";
+import Items from "./Items";
+import { nanoid } from "nanoid";
 
 const App = () => {
 	const [items, setItems] = useState([]);
+
+	const addItem = (itemName) => {
+		const newItem = {
+			name: itemName,
+			completed: false,
+			id: nanoid(),
+		};
+		setItems([...items, newItem]);
+	};
+
+	const removeItem = (itemId) => {};
+
 	return (
 		<section className="section-center">
-			<Form />
+			<Form addItem={addItem} />
+			<Items items={items} removeItem={removeItem} />
 		</section>
 	);
 };
