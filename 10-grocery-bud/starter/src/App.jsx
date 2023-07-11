e8e1a6fe1ad220dcb708f036fd8001018442c1e4
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-07-10 18:40:08 -0700

    Grab local storage values on load

--- 10-grocery-bud/starter/src/App.jsx
+++ 10-grocery-bud/starter/src/App.jsx
@@ -3,11 +3,22 @@ import Form from "./Form";
 import Items from "./Items";
 import { nanoid } from "nanoid";
 
+const getLocalStorage = () => {
+	let list = localStorage.getItem("list");
+	if (list) {
+		list = JSON.parse(localStorage.getItem("list"));
+	} else {
+		list = [];
+	}
+	return list;
+};
+
 const setLocalStorage = (items) => {
 	localStorage.setItem("list", JSON.stringify(items));
 };
 
 const App = () => {
+	getLocalStorage();
 	const [items, setItems] = useState([]);
 
 	const addItem = (itemName) => {
