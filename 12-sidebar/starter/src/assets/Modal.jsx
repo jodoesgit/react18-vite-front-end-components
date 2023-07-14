daf6e14c7bc924565a5f939a52e0f3e72daf065b
jodoesgit <122350666+jodoesgit@users.noreply.github.com>
2023-07-14 15:49:24 -0700

    Added Modal styles

--- 12-sidebar/starter/src/assets/Modal.jsx
+++ 12-sidebar/starter/src/assets/Modal.jsx
@@ -1,4 +1,17 @@
+import { FaTimes } from "react-icons/fa";
+import { useGlobalContext } from "./context";
+
 const Modal = () => {
-	return <div>Modal</div>;
+	const { isModal, closeModal } = useGlobalContext();
+	return (
+		<div className="modal-overlay">
+			<div className="modal-container">
+				<h3>Content</h3>
+				<button className="close-modal-btn" onClick={closeModal}>
+					<FaTimes />
+				</button>
+			</div>
+		</div>
+	);
 };
 export default Modal;
